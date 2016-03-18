af9f5d2e13859428eb32217a63df3a481b4318ba
DriesVH <dries.vanhertbruggen@hotmail.com>
2016-03-18 09:49:14 +0100

    Update db.php

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
 /* Attempt MySQL server connection. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
- = mysqli_connect("localhost", "boyd", "boyd", "ap");
+$link = mysqli_connect("localhost", "boyd", "boyd", "ap");
  
 // Check connection
 if($link === false){
